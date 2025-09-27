9e84da2027b8f2535bb389ca3a499a90fc45d9e5
Sanskriti41 <sanskriti761@gmail.com>
2025-09-27 16:46:18 +0530

    acceptiong data in params and body

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ import express from "express"
 import dotenv from "dotenv"
 const app = express();
 dotenv.config();
-const port = process.env.PORT
+const port = process.env.PORT;
+
+app.use(express.json());
 
 
-app.listen(port,()=>
+
+//middlewares
+//frontend --> middleware(stringify) -->backend(APi)
+app.get('/' , (req,res)=>{
+    console.log(req.body);
+    res.send("xdfxydtf")
+})
+console.log(port)
+
+app.listen(port ,()=>
 {
     console.log(`listening on port ${port}`)
 })
